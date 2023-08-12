cd4f4678ae76c257935fb2be64a945effbf2f1ce
ethancarlsson <ethanmcarlsson@gmail.com>
2023-08-12 09:01:18 +0200

    Add utilitiy plugins

---
+++ lua/plugins/utility.lua
@@ -0,0 +1,67 @@
+return {
+	'tpope/vim-surround',
+	'tpope/vim-sleuth',
+	  -- Useful plugin to show you pending keybinds.
+	{ 'folke/which-key.nvim', opts = {} },
+	{
+		-- Set lualine as statusline
+		'nvim-lualine/lualine.nvim',
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				component_separators = '|',
+				section_separators = '',
+			},
+		},
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			char = '┊',
+			show_end_of_line = true,
+			show_current_context = true,
+			space_char_blankline = " ",
+			char_highlight_list = {
+				"IndentBlanklineIndent1",
+				"IndentBlanklineIndent2",
+				"IndentBlanklineIndent3",
+				"IndentBlanklineIndent4",
+				"IndentBlanklineIndent5",
+				"IndentBlanklineIndent6",
+			},
+
+		}
+	},
+	-- "gc" to comment visual regions/lines
+	{
+		'numToStr/Comment.nvim',
+		opts = {
+			toggler = {
+				---Line-comment toggle keymap
+				line = '<space>/',
+				---Block-comment toggle keymap
+				block = '<space>?',
+			},
+			---LHS of operator-pending mappings in NORMAL and VISUAL mode
+			opleader = {
+				---Line-comment keymap
+				line = '<space>/',
+				---Block-comment keymap
+				block = '<space>?',
+			},
+			---LHS of extra mappings
+			extra = {
+				---Add comment on the line above
+				above = '<space>/O',
+				---Add comment on the line below
+				below = '<space>/o',
+				---Add comment at the end of line
+				eol = '<space>/a',
+			},
+
+		}
+	},
+
+}
