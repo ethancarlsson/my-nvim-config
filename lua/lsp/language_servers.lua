e1c3b8a2994ff36a2677bc99d21d9bb2f32e3924
ethancarlsson <ethanmcarlsson@gmail.com>
2023-09-23 08:57:40 +0200

    Remove unused language servers

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,9 +1,2 @@
 return {
-    phpactor = {},
-    lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-        },
-    },
 }
