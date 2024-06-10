46005fd08e1330c6ff6f2a5e469f011b79296e77
ethancarlsson <ethanmcarlsson@gmail.com>
2024-06-10 20:49:36 +0200

    Make actions available from vistual mode

--- lua/lsp/on_attach.lua
+++ lua/lsp/on_attach.lua
@@ -16,6 +16,7 @@ local on_attach = function(_, bufnr)
 
 	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 	nmap("<leader>a", vim.lsp.buf.code_action, "Code [A]ction")
+	vim.keymap.set("v", "<leader>a", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code [A]ction" })
 
 	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
